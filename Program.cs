aab25efa57d399d092ee0b12fddeab6dd8db6ecd
cantsea <cantseasharp@gmail.com>
2026-03-02 23:46:13 -0500

    name refactoring, map upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using template;
+using ev;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
