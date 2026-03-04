8ad72cb55e9800141981f3b02716a52dac90c7be
cantsea <cantseasharp@gmail.com>
2026-03-03 22:01:10 -0500

    chapters, characters update

--- Models/MapData.cs
+++ Models/MapData.cs
@@ -4,6 +4,7 @@ public class MapData
 {
     public int TotalPages { get; set; } = 1000;
     public List<CharacterModel> Characters { get; set; } = new();
+    public List<ChapterModel> Chapters { get; set; } = new();
     public List<MapEntry> Entries { get; set; } = new();
 }
 
@@ -12,6 +13,16 @@ public class CharacterModel
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; } = "";
     public string Color { get; set; } = "#3b82f6";
+}
+
+public class ChapterModel
+{
+    public int Number { get; set; }
+    public string Name { get; set; } = "";
+    public int Page { get; set; }
+
+    public double GetProgress(int totalPages) =>
+        totalPages > 0 ? Math.Round((double)Page / totalPages, 4) : 0;
 }
 
 public class MapEntry
@@ -23,4 +34,5 @@ public class MapEntry
     public double X { get; set; }
     public double Y { get; set; }
     public string Note { get; set; } = "";
+    public bool IsInvisible { get; set; } = false;
 }
